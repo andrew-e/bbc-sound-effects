347af118a81d55c286fb7b2b2a3a180a1ad9a52f
Andrew Elmore <andrew@movivo.com>
2018-04-19 18:20:36 +0100

    Bringing in the csv locally and wrapping it in some uggggllllyyyy retry logic

--- index.js
+++ index.js
@@ -5,46 +5,63 @@ const http = require('http');
 const fs = require('fs');
 
 const lastDownloadedFile = 'sounds/lastFileDownloaded.txt';
+const soundEffectCSV = fs.readFileSync('BBCSoundEffects.csv', 'utf8');
 
-return request({
-    method: 'GET',
-    uri: 'http://bbcsfx.acropolis.org.uk/assets/BBCSoundEffects.csv',
-    timeout: 30000,
-})
-.then(soundEffectCSV => {
-    let samples = parse(soundEffectCSV, {columns: true});
-    const lastDownloaded = fs.readFileSync(lastDownloadedFile, 'utf8');
+let samples = parse(soundEffectCSV, {columns: true});
+const lastDownloaded = fs.readFileSync(lastDownloadedFile, 'utf8');
 
-    console.log('last = ' + lastDownloaded);
+return downloadStuff()
+.catch(err => {
+    retry(10, function () { return downloadStuff(); });
+});
 
-    if (lastDownloaded) {
-        let slicePoint = 0;
-        samples.forEach((sample, i) => {
+function downloadStuff() {
+    return new Promise(resolve => resolve())
+    .then(() => {
+        return soundEffectCSV;
+    })
+    .then(soundEffectCSV => {
+        let samples = parse(soundEffectCSV, {columns: true});
+        const lastDownloaded = fs.readFileSync(lastDownloadedFile, 'utf8');
 
-            if (sample.location === lastDownloaded) {
-                slicePoint = i+1;
+        console.log('last = ' + lastDownloaded);
+
+        if (lastDownloaded) {
+            let slicePoint = 0;
+            samples.forEach((sample, i) => {
+
+                if (sample.location === lastDownloaded) {
+                    slicePoint = i+1;
+                }
+            });
+
+            samples = samples.slice(slicePoint);
+        }
+
+        samples.forEach(sample => {
+            const description = sample.description.replace(/[/\\?%*:|"<>.]/g, '');
+            const filename = `sounds/${description}${sample.location}`;
+
+            console.log(`downloading then saving ${filename}`);
+
+            if (!fs.existsSync(filename)) {
+                //yes this is a sync http call, this is so the BBC doesn't get angry at us for demolishing their servers
+                const response = syncRequest('GET', `http://bbcsfx.acropolis.org.uk/assets/${sample.location}`);
+
+                fs.writeFileSync(filename, response.getBody());
+                fs.writeFileSync(lastDownloadedFile, sample.location);
             }
         });
+    })
+}
 
-        samples = samples.slice(slicePoint);
-    }
-
-    samples.forEach(sample => {
-        const description = sample.description.replace(/[/\\?%*:|"<>.]/g, '');
-        const filename = `sounds/${description}${sample.location}`;
-
-        console.log(`downloading then saving ${filename}`);
-
-        if (!fs.existsSync(filename)) {
-            //yes this is a sync http call, this is so the BBC doesn't get angry at us for demolishing their servers
-            const response = syncRequest('GET', `http://bbcsfx.acropolis.org.uk/assets/${sample.location}`);
-
-            fs.writeFileSync(filename, response.getBody());
-            fs.writeFileSync(lastDownloadedFile, sample.location);
+function retry(maxRetries, fn) {
+    return fn().catch(function(err) {
+        console.log("Oops, something wen't wrong");
+        if (maxRetries <= 0) {
+            throw err;
         }
+        console.log(`${maxRetries} retries left`);
+        return retry(maxRetries - 1, fn);
     });
-})
-.catch(err => {
-    console.error("Oops, something wen't wrong");
-    console.error(JSON.stringify(err));
-});
+}
